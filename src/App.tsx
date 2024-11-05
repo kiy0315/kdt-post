7bac556a66d0c07f726dba8a9401d4e462e3c902
DESKTOP-HUQ5D35\김인영 <ekseh789@gmail.com>
2024-11-05 16:35:32 +0900

    fix: dropDownForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
 import { useState } from 'react'
 import { appContainer, board, buttons } from './App.css'
 import reactLogo from './assets/react.svg'
+import BoardList from './comonents/BoardList/BoardList'
+import ListsContainer from './comonents/ListsContainer/ListsContainer'
+import { useTypedDispatch, useTypedSelector } from './hooks/redux'
 import viteLogo from '/vite.svg'
 
 function App() {
+  const [activeBoardId, setActiveBoardId] = useState('board-0');
+
+  const boards = useTypedSelector(state => state.boards.boardArray);
+
+  const getActiveBoard = boards.filter(board => board.boardId === activeBoardId)[0];
+
+  const lists = getActiveBoard.lists;
+
   return (
     <div className={appContainer}>
+      <BoardList activeBoardId={activeBoardId}
+        setActiveBoardId={setActiveBoardId} />
       <div className={board}>
-
+        <ListsContainer lists={lists} boardId ={getActiveBoard.boardId}/>
       </div>
-      <div>
-        <button className={buttons}>
+      <div className={buttons}>
+        <button >
           이 게시판 삭제하기
         </button>
         <button>
